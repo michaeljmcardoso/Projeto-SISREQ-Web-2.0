1515ba014fd4951698f04efa9be37545c8175054
Michael <michaelmiranda38@yahoo.com.br>
2024-12-07 11:37:25 -0300

    inclui bibliotecas necessarias

---
+++ sisreq_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import hashlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from datetime import datetime
+
+
+st.title("Sistema de Banco de Dados")
