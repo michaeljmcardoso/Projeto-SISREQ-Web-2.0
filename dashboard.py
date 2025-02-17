5b444455e1b949b11fafc4f7a9365eb37b2cd6fd
michaeljmcardoso <michaelmiranda38@yahoo.com.br>
2025-02-17 12:08:13 -0300

    inclui Ano de Abertura dos processo no Dashboard e geolocalização das comunidades

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import streamlit as st
 import sqlite3
-from constantes import FASE_PROCESSO
+import webbrowser
 import matplotlib.pyplot as plt
+import plotly.express as px
 import seaborn as sns
+from constantes import FASE_PROCESSO
 
 # Função para conectar ao banco de dados
 def conectar_banco_de_dados():
@@ -163,3 +165,120 @@ def processos_por_municipio():
         st.pyplot(fig)
     else:
         st.warning("Não há registros para exibir.")
+
+def exibir_processos_por_data_abertura():
+    st.write("### Ano de Abertura dos Processos")
+    conn = conectar_banco_de_dados()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT Data_Abertura, COUNT(*) AS Num_Processos FROM processos GROUP BY Data_Abertura")
+    resultados = cursor.fetchall()
+
+    if resultados:
+        datas_abertura = []
+        num_processos = []
+
+        for resultado in resultados:
+            datas_abertura.append(resultado[0])
+            num_processos.append(resultado[1])
+
+        # Criar DataFrame
+        data = pd.DataFrame({'Data de Abertura': datas_abertura, 'Número de Processos': num_processos})
+
+        # Converter a coluna de datas para o formato apropriado (opcional)
+        data['Data de Abertura'] = pd.to_datetime(data['Data de Abertura'], format="%d-%m-%Y")
+
+        # Ordenar o DataFrame por data de abertura
+        data = data.sort_values(by='Data de Abertura')
+
+        # Calcular o acumulado de processos por ano
+        data['Ano'] = data['Data de Abertura'].dt.year
+        acumulado_por_ano = data.groupby('Ano')['Número de Processos'].cumsum()
+
+        # Adicionar a coluna de acumulado de processos ao DataFrame
+        data['Acumulado'] = acumulado_por_ano
+
+        # Plot do gráfico de linhas
+        fig, ax = plt.subplots(figsize=(12, 6))
+        sns.lineplot(x='Data de Abertura', y='Acumulado', data=data, palette="Set1")
+
+        # Configurações do gráfico
+        ax.set(title='Acumulado de Processos por Ano')
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        sns.set_style("white")
+        sns.despine(right=True, top=True, bottom=False, left=False)
+        plt.xticks(rotation=45)  # Rotacionar os rótulos do eixo x para melhor legibilidade
+
+        plt.tight_layout()
+        st.pyplot(fig)
+    else:
+        st.warning('Não há registros para exibir.', title='Erro')
+
+def plotar_mapa_interativo():
+    st.write("### Geolocalização das Comunidades")
+    if st.button("Plotar Mapa"):
+        conn = conectar_banco_de_dados()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT Municipio, Comunidade, Latitude, Longitude, Num_Familias FROM processos")
+        resultados = cursor.fetchall()
+
+        if resultados:
+            municipios = []
+            comunidades = []
+            latitudes = []
+            longitudes = []
+            numero_de_familias = []
+
+            for resultado in resultados:
+                municipios.append(resultado[0])
+                comunidades.append(resultado[1])
+
+                # Verificar se os valores de latitude e longitude são numéricos antes de adicioná-los
+                if isinstance(resultado[2], (float, int)) and isinstance(resultado[3], (float, int)):
+                    latitudes.append(resultado[2])
+                    longitudes.append(resultado[3])
+                else:
+                    latitudes.append(None)  # Se não for numérico, adicione None
+                    longitudes.append(None)
+
+                numero_de_familias.append(resultado[4])
+
+            df = pd.DataFrame({
+                'Municipio': municipios, 
+                'Comunidade': comunidades, 
+                'Latitude': latitudes, 
+                'Longitude': longitudes, 
+                'Num_Familias': numero_de_familias
+            })
+
+            # Filtrar linhas com valores não nulos nas colunas de latitude e longitude
+            df = df.dropna(subset=['Latitude', 'Longitude'])
+
+            if not df.empty:
+                px.set_mapbox_access_token('pk.eyJ1IjoibWpkYXRhc2NpZW5jZSIsImEiOiJjbGFlY3hwbGcwbWlxM3Nxa2NuOWh4cmNzIn0.2ye_ghCe_WAgIpqueUqedA')
+
+                fig = px.scatter_mapbox(
+                    df, 
+                    lat='Latitude', 
+                    lon='Longitude',
+                    color=df['Municipio'],
+                    color_discrete_sequence=["fuchsia"],
+                    size_max=15,
+                    zoom=10,
+                    hover_name='Comunidade',
+                    hover_data='Num_Familias',
+                    height=700,
+                )
+
+                fig.update_layout(mapbox_style="streets")
+                fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+                fig.write_html('mapa_interativo.html')
+
+                webbrowser.open('mapa_interativo.html')
+
+            else:
+                st.warning('Não há registros válidos para exibir.', title='Erro')
+        else:
+            st.warning('Não há registros para exibir.', title='Erro')
--- pagina_dashboard.py
+++ pagina_dashboard.py
@@ -1,8 +1,11 @@
 import streamlit as st
-from dashboard import processos_por_fase, processos_por_municipio, grafico_processos_por_fase
+from dashboard import processos_por_fase, processos_por_municipio, grafico_processos_por_fase, plotar_mapa_interativo, exibir_processos_por_data_abertura
 
 def dashboard(fase):
     st.markdown('<h2 style="color: #1f77b4;">Dashboard SISREQ</h2>', unsafe_allow_html=True)
     processos_por_municipio()
     processos_por_fase()
     grafico_processos_por_fase()
+    exibir_processos_por_data_abertura()
+    plotar_mapa_interativo()
+
