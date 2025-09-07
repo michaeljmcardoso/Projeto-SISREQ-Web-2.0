922e81cc375b545242814f76b497b2e04d152971
michaeljmcardoso <michaelmiranda38@yahoo.com.br>
2025-09-06 23:20:08 -0300

    refatora pagina inicial

--- pagina_pesquisa.py
+++ pagina_pesquisa.py
@@ -3,6 +3,7 @@ import pandas as pd
 import io
 import streamlit as st
 from constantes import MUNICIPIOS, TIPO_SOBREPOSICAO
+from obter_todos_registros import obter_todos_os_registros
 
 #def filtrar_por_comunidade():
 
@@ -218,5 +219,84 @@ def salvar_extrato_planilha(df, nome_arquivo):
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
     st.success(f"Extrato para {nome_arquivo} salvo e pronto para download!")
+
+def pesquisar_comunidade():
+    df = obter_todos_os_registros()
+    if not df.empty:
+        if 'ID' in df.columns:
+            df = df.drop(columns=['ID'])
+            df.index = df.index + 1
+    # Criar identificadores únicos de comunidade e município
+    df['Comunidade_Municipio'] = df['Comunidade'] + " - " + df['Municipio']
+    comunidades_disponiveis = df['Comunidade_Municipio'].unique().tolist()
+
+    # Caixa de seleção para escolha da comunidade
+    st.markdown('<h5 style="color: #1f77b4;">Selecione uma Comunidade ou Município:</h5>', unsafe_allow_html=True)
+    comunidade_selecionada = st.selectbox(" ", options=comunidades_disponiveis)
+
+    # Validar a seleção da comunidade
+    if comunidade_selecionada:
+        comunidade, municipio = comunidade_selecionada.split(" - ")
+        registros_filtrados = df[(df['Comunidade'] == comunidade) & (df['Municipio'] == municipio)]
+
+        if not registros_filtrados.empty:
+            #st.markdown(f"<p style='color: #1f77b4;'><strong>Detalhes do Processo</strong></p>", unsafe_allow_html=True)
+
+            for index, registro in registros_filtrados.iterrows():
+                st.markdown(
+                    f"<p style='color: #FFFFFF; background-color: #1f77b4; padding: 1px; border-radius: 1px;'>",
+                    unsafe_allow_html=True
+                )
+
+                # Divisão em colunas para exibição de dados
+            col1, col2, col3, col4 = st.columns(4)
+
+            # Coluna 1
+            with col1:
+                st.markdown(f"<p><strong>Número do Processo:</strong> {registro[0]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Data de Abertura:</strong> {registro[1]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Comunidade:</strong> {registro[2]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Município:</strong> {registro[3]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Número de Famílias:</strong> {registro[5]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Área Identificada (ha):</strong> {registro[4]}</p>", unsafe_allow_html=True)
+
+            # Coluna 2
+            with col2:
+                st.markdown(f"<p><strong>Fase do Processo:</strong> {registro[6]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Etapa RTID:</strong> {registro[7]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Relatório Antropológico:</strong> {registro[15]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Certidão FCP:</strong> {registro[18]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Data de Certificação:</strong> {registro[19]}</p>", unsafe_allow_html=True)
+
+            # Coluna 3
+            with col3:
+                st.markdown(f"<p><strong>Área Titulada (ha):</strong> {registro[12]}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Título:</strong> {registro['Titulo']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>PNRA:</strong> {registro['PNRA']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Latitude:</strong> {registro['Latitude']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Longitude:</strong> {registro['Longitude']}</p>", unsafe_allow_html=True)
+
+            # Coluna 4
+            with col4:
+                st.markdown(f"<p><strong>Edital DOU:</strong> {registro['Edital_DOU']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Edital DOE:</strong> {registro['Edital_DOE']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Portaria DOU:</strong> {registro['Portaria_DOU']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Decreto DOU:</strong> {registro['Decreto_DOU']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Sobreposição Territorial:</strong> {registro['Sobreposicao']}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p><strong>Detalhes de Sobreposição:</strong> {registro['Analise_de_Sobreposicao']}</p>", unsafe_allow_html=True)
+           
+        else:
+            st.warning("Comunidade não encontrada. Por favor, verifique o nome informado.")
+
+    # informações adicionais
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.markdown(f"<p><strong>Ação Civil Pública:</strong> {registro['Acao_Civil_Publica']}</p>", unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"<p><strong>Data da Sentença:</strong> {registro['Data_Decisao']}</p>", unsafe_allow_html=True)
+    with col3:
+        st.markdown(f"<p><strong>Teor/Prazo da Sentença:</strong> {registro[24]}</p>", unsafe_allow_html=True)
+    with col4:
+        st.markdown(f"<p><strong>Outras Informações:</strong> {registro['Outras_Informacoes']}</p>", unsafe_allow_html=True)
 
 
