62b68d89c88ca043c50a91ef928fdcc2e554d888
michaeljmcardoso <michaelmiranda38@yahoo.com.br>
2025-09-26 12:55:07 -0300

    ajuste do modelo gemini pra 2.5 flash

--- pagina_chat.py
+++ pagina_chat.py
@@ -24,7 +24,7 @@ def iniciar_chat():
     }
 
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.5-flash",
         generation_config=generation_config,
         system_instruction=(
             "Você é o assistente virtual Oráculo especialista em processos de regularização fundiária de territórios quilombolas "
