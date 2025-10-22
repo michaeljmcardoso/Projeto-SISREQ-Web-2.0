f5d8c51ec8c5f7d8c15d9f9f19cce7aa5e3d7874
michaeljmcardoso <michaelmiranda38@yahoo.com.br>
2025-10-22 08:31:18 -0300

    tira o coração do sidebar

--- sisreq_app.py
+++ sisreq_app.py
@@ -329,5 +329,5 @@ else:
    
     st.sidebar.markdown('''
     ---
-    Created with ❤️ by [Michael](https://github.com/michaeljmcardoso).
+    Created with by [Michael](https://github.com/michaeljmcardoso).
     ''')  
